51ec07df299fd6c18810b363fdcca48451feb12a
arju10 <arjuarju71@gmail.com>
2023-07-13 15:40:30 +0600

    apply authmiddleware in current-user => create current-user controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { registerController, loginController } = require("../controllers/authController");
+const { registerController, loginController, currentUserController} = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 //routes
@@ -9,5 +10,7 @@ router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
 
+//GET CURRENT USER || GET
+router.get("/current-user", authMiddleware, currentUserController);
 
 module.exports = router;
